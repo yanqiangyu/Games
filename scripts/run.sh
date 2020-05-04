c81a30eeb406abb2b72645d0a3e581141e7da812
Steve Yu <syu@ialogic.com>
2020-05-03 20:22:11 -0400

    scripts to stop/restart

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 nohup java -cp ../lib/http-20070405.jar:../lib/pig.jar com.ialogic.games.cards.server.CardHttpServer > ../log/card.log 2>&1 &
+ps aux | grep com.ialogic.games.cards.server.CardHttpServer | grep java
