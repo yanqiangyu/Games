1d742600a11bc79196e4e196308443a6b054c374
Steve <steve@ialogic.com>
2020-05-09 09:43:08 -0400

    save log files when restart

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+SCRIPTDIR=`dirname $0`
+pushd  $SCRIPTDIR
+echo "========================================"
+mv ../log/card.log ../log/card.log.`date +"%Y%m%d-%H%M%S"`
+echo "LOG Saved"
+ls -lrt ../log | tail -1f
+echo "========================================"
 nohup java -cp ../lib/http-20070405.jar:../lib/pig.jar com.ialogic.games.cards.server.CardHttpServer > ../log/card.log 2>&1 &
 ps aux | grep com.ialogic.games.cards.server.CardHttpServer | grep java
+popd
