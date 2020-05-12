4ed7884f271fce34f4efe565d0aad9366c2e515a
Steve <steve@ialogic.com>
2020-05-12 13:00:04 -0400

    Standalone

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,7 @@ mv ../log/card.log ../log/card.log.`date +"%Y%m%d-%H%M%S"`
 echo "LOG Saved"
 ls -lrt ../log | tail -1f
 echo "========================================"
-nohup java -cp ../lib/http-20070405.jar:../lib/pig.jar com.ialogic.games.cards.server.CardHttpServer > ../log/card.log 2>&1 &
+cd ../
+nohup java -cp lib/http-20070405.jar:lib/pig.jar com.ialogic.games.cards.server.CardHttpServer > log/card.log 2>&1 &
 ps aux | grep com.ialogic.games.cards.server.CardHttpServer | grep java
 popd
