d3ffa5bf79cb003c705d5d57e48c02b740410cd8
Steve <steve@ialogic.com>
2020-05-12 13:12:39 -0400

    minor build stuff

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,5 @@ echo "Compiling java source code..."
 javac -d ../classes/ `find ../src -name *.java`
 echo "done"
 echo "jar -cfv ../lib/pig.jar -C ../classes/ com"
-jar -cfv ../lib/pig.jar -C ../classes/ com 
+jar -cfv ../lib/pig.jar -C ../classes/ com  > build_jar.out
 echo "done"
