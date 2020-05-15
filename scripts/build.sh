88a09c4f223e884eeac86ec8e2e37fbb7892002b
Steve <steve@ialogic.com>
2020-05-14 23:18:07 -0400

    Fixed build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo "Compiling java source code..."
-javac -d ../classes/ `find ../src -name *.java`
+DIRSEP=:
+if [ "$OS" == "Windows_NT" ]
+then
+	DIRSEP=";"
+fi
+javac -cp "../lib/http-20070405.jar$DIRSEP../lib/javax.json-1.1.jar" -d ../classes/ `find ../src -name *.java`
 echo "done"
 echo "jar -cfv ../lib/pig.jar -C ../classes/ com"
 jar -cfv ../lib/pig.jar -C ../classes/ com  > build_jar.out
