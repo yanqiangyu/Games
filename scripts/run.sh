6cf91d546f4d201fe9b084983d58e5213235e843
Steve Yu <syu@ialogic.com>
2020-05-16 12:03:36 -0400

    run.sh added jason jar

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,6 +7,6 @@ echo "LOG Saved"
 ls -lrt ../log | tail -1f
 echo "========================================"
 cd ../
-nohup java -cp lib/http-20070405.jar:lib/pig.jar com.ialogic.games.cards.server.CardHttpServer > log/card.log 2>&1 &
+nohup java -cp lib/http-20070405.jar:lib/pig.jar:lib/javax.json-1.1.jar com.ialogic.games.cards.server.CardHttpServer > log/card.log 2>&1 &
 ps aux | grep com.ialogic.games.cards.server.CardHttpServer | grep java
 popd
